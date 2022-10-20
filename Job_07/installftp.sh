40f26ef4c9b3bc3478bfa9ce19b3e4e212a85e6b
clement-crippa <clement.crippa@laplateforme.io>
2022-10-20 09:57:15 +0200

    Ajout de la complétion automatique du certificat SSL

--- Job_07/installftp.sh
+++ Job_07/installftp.sh
@@ -3,16 +3,24 @@ apt-get -y upgrade
 apt-get install -y proftpd-* openssl sudo
 sudo useradd -m Merry -p "$(echo "kalimac" | openssl passwd -1 -stdin)"
 sudo useradd -m Pippin -p "$(echo "secondbreakfast" | openssl passwd -1 -stdin)"
-mkdir /etc/proftpd/ssl
-mkdir /etc/configproftpd
-cp ./proftpd.conf /etc/configproftpd/proftpd.conf
-cp ./tls.conf /etc/configproftpd/tls.conf
-cp ./modules.conf /etc/configproftpd/modules.conf
-ln -sf /etc/configproftpd/proftpd.conf /etc/proftpd/proftpd.conf
-ln -sf /etc/configproftpd/tls.conf /etc/proftpd/tls.conf
-ln -sf /etc/configproftpd/modules.conf /etc/proftpd/modules.conf
+sudo mkdir /etc/proftpd/ssl
+sudo mkdir /etc/configproftpd
+sudo cp ./proftpd.conf /etc/configproftpd/proftpd.conf
+sudo cp ./tls.conf /etc/configproftpd/tls.conf
+sudo cp ./modules.conf /etc/configproftpd/modules.conf
+sudo ln -sf /etc/configproftpd/proftpd.conf /etc/proftpd/proftpd.conf
+sudo ln -sf /etc/configproftpd/tls.conf /etc/proftpd/tls.conf
+sudo ln -sf /etc/configproftpd/modules.conf /etc/proftpd/modules.conf
 echo Création du lien symbolique des config;
-sudo openssl req -new -x509 -keyout /etc/proftpd/ssl/proftpd.key.pem -days 365 -nodes -out /etc/proftpd/ssl/proftpd.cert.pem
+sudo openssl req -new -x509 -keyout /etc/proftpd/ssl/proftpd.key.pem -days 365 -nodes -out /etc/proftpd/ssl/proftpd.cert.pem <<EOF
+FR
+France
+Marseille
+.
+.
+.
+.
+EOF
 echo Configuration terminé.;
 sudo chmod 640 /etc/proftpd/ssl/proftpd.key.pem
 sudo chmod 640 /etc/proftpd/ssl/proftpd.cert.pem
