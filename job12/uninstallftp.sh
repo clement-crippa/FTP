6ddd342153cec656bf8caa8bc2f6f4c936eac9a0
clement-crippa <clement.crippa@laplateforme.io>
2023-05-12 13:51:06 +0200

    Fix commentaire

--- job12/uninstallftp.sh
+++ job12/uninstallftp.sh
@@ -1,10 +1,10 @@
-# Arrêt des serveurs
+##Arrêt des serveurs
 sudo systemctl stop bind9
 sudo systemctl stop proftpd
-# Désinstallation des serveurs et de leur configuration
+##Désinstallation des serveurs et de leur configuration
 apt-get remove -y --purge proftpd* openssl bind9
 rm -r /etc/configproftpd
-# Supression des utilisateurs
+##Supression des utilisateurs
 sudo deluser --remove-home Merry
 sudo deluser --remove-home Pippin
 apt-get remove -y --purge sudo
